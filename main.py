de12cd68f51e428e315cbf2839039f3bbaf485d3
larisamariabota <larisa.bota.15@gmail.com>
2025-11-30 21:06:14 +0200

    inceput folder pentru comenzii

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from loader import load_file
-from grouping.profile_by_ip import profile_by_ip
-from printer import print_ip_profiles
 
 if __name__ == "__main__":
-    lista = load_file("test_logs/apache_300.log")
-    grouped = profile_by_ip(lista)
-    print_ip_profiles(grouped)
+    entries = load_file("test_logs/mixed_50.log")
+
+    for e in entries:
+        print("="*60)
+        for key, value in e.items():
+            print(f"{key:12}: {value}")
+
 
 
  
